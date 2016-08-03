f9d8d7e8790503cb33f13b12979eef7d56058875
Alex Macleod <alex@macleod.io>
2016-08-03 18:35:15 +0100

    add test editor decorations

--- client/app.js
+++ client/app.js
@@ -1,9 +1,11 @@
 /// <reference path="../node_modules/monaco-editor/monaco.d.ts" />
 
-require.config({ paths: { 'vs': '../node_modules/monaco-editor/dev/vs' }})
+require.config({ paths: { 'vs': '../node_modules/monaco-editor/dev/vs' } })
 require(['vs/editor/editor.main'], main)
 
-function main () {
+var state = new Object()
+
+function main() {
   var editor = monaco.editor.create(document.getElementById('editor'), {
     value: '/// test\nfunction test () {\n  return \'hello world\'\n}',
     language: 'javascript'
@@ -13,20 +15,45 @@ function main () {
     editor.layout()
   })
 
-  var model = editor.getModel()
-  var matches = model.findMatches('test', false, false, false, false)
-
-  editor.onKeyDown(onEnter)
-
-  console.table(matches)
+  editor.onKeyDown(function (event) {
+    onEnter(event, editor)
+  })
 
   // DEBUG
   window.editor = editor
   window.model = model
 }
 
-function onEnter (event) {
+/**
+ * @param {monaco.IKeyboardEvent} event
+ * @param {monaco.editor.IStandaloneCodeEditor} editor
+ */
+function onEnter(event, editor) {
   if (!event.altKey || event.browserEvent.code !== 'Enter') return
 
-  console.log('!!!!')
+  var model = editor.getModel()
+  // var oldDecorations = model.getAllDecorations()
+  //   .map(function (decoration) {
+  //     return decoration.id
+  //   })
+
+  var oldDecorations = state.oldDecorations || []
+
+  var newDecorations = model.findMatches('test', false, false, false, false)
+    .map(function (range) {
+      return {
+        range: {
+          startLineNumber: range.startLineNumber,
+          startColumn: 1,
+          endLineNumber: range.endLineNumber,
+          endColumn: 1
+        },
+        options: {
+          linesDecorationsClassName: 'testDecoration',
+          isWholeLine: true
+        }
+      }
+    })
+
+  state.oldDecorations = model.deltaDecorations(oldDecorations, newDecorations)
 }
