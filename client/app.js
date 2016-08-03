0efa4c7b8228ef9e8ac23c6f7820c8a6b590d9e6
Alex Macleod <alex@macleod.io>
2016-08-03 22:54:01 +0100

    naïve function highlighting

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ require(['vs/editor/editor.main'], main)
 
 var state = new Object()
 
-function main() {
+function main () {
   var editor = monaco.editor.create(document.getElementById('editor'), {
     value: '/// test\nfunction test () {\n  return \'hello world\'\n}',
     language: 'javascript'
@@ -16,30 +16,25 @@ function main() {
   })
 
   editor.onKeyDown(function (event) {
-    onEnter(event, editor)
+    if (!event.altKey || event.browserEvent.code !== 'Enter') return
+    onEnter(editor)
   })
 
   // DEBUG
+  window.monaco = monaco
   window.editor = editor
-  window.model = model
+  window.model = editor.getModel()
 }
 
 /**
- * @param {monaco.IKeyboardEvent} event
  * @param {monaco.editor.IStandaloneCodeEditor} editor
  */
-function onEnter(event, editor) {
-  if (!event.altKey || event.browserEvent.code !== 'Enter') return
-
+function onEnter (editor) {
   var model = editor.getModel()
-  // var oldDecorations = model.getAllDecorations()
-  //   .map(function (decoration) {
-  //     return decoration.id
-  //   })
 
   var oldDecorations = state.oldDecorations || []
 
-  var newDecorations = model.findMatches('test', false, false, false, false)
+  var newDecorations = model.findMatches('///\\s*test', false, true, false, false)
     .map(function (range) {
       return {
         range: {
@@ -50,10 +45,37 @@ function onEnter(event, editor) {
         },
         options: {
           linesDecorationsClassName: 'testDecoration',
-          isWholeLine: true
+          isWholeLine: true,
+          stickiness: monaco.editor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges
         }
       }
     })
 
   state.oldDecorations = model.deltaDecorations(oldDecorations, newDecorations)
+}
+
+/**
+ * @param {monaco.editor.IModel} model
+ * @param {number} startLine
+ */
+function highlightFunction (model, startLine) {
+  var endLine = startLine - 1
+  var eof = model.getFullModelRange().endLineNumber
+
+  var braces = 0
+
+  // TODO: replace with IModel.tokenIterator
+
+  do {
+    var line = model.getLineContent(++endLine)
+    var char = line.length
+
+    while (char--) {
+      if (line[char] === '{') braces++
+      else if (line[char] === '}') braces--
+    }
+  } while (braces && endLine <= eof)
+
+  var range = new monaco.Range(startLine, 1, endLine, 1)
+  return range
 }
